4d5cfc1fc0f08bd27bdad37b73b64bbf8233e024
JenieX <sozorablack@gmail.com>
2025-05-26 05:08:45 +0300

    🧹 update to support all browsers

--- playground/dist/playground.dev.js
+++ playground/dist/playground.dev.js
@@ -12,18 +12,61 @@
 // @compatible     edge Violentmonkey
 // @supportURL     https://github.com/JenieX/user-js-next/issues
 // @homepageURL    https://github.com/JenieX/user-js-next/tree/main/playground
-// @icon           https://violentmonkey.github.io/favicon-32x32.png
+// @icon           https://github.com/JenieX/assets/raw/main/icons/violentmonkey-beta.png
 // @license        MIT
 // ==/UserScript==
 
-let url = 'http://192.168.0.39:1013/playground';
+const isTor = sessionStorage.getItem('isTor') === 'true';
 
-if (navigator.userAgent.includes('Firefox')) {
-  const exposerUUID = sessionStorage.getItem('exposerUUID').slice(0, -1);
-  url = exposerUUID + '/playground/dist/playground.bundle.js';
+const exposerUUID = sessionStorage.getItem('exposerUUID')?.slice(0, -1);
+const isExposed = exposerUUID !== undefined;
+
+let bundleURL = 'http://192.168.0.39:1013/playground';
+
+if (isExposed) {
+  bundleURL = `${exposerUUID}/playground/dist/playground.bundle.js`;
 }
 
-fetch(url)
-  .then((response) => response.text())
-  .then((responseText) => eval(responseText))
-  .catch((exception) => console.error(exception));
+fetch(bundleURL)
+  .then(async (response) => response.text())
+  .then(async (code) => {
+    if (isExposed) {
+      let sourceMapURL = bundleURL.replace('bundle.js', 'source.map');
+
+      const needle = /(MappingURL=)([^?]+)([^\n]+)/;
+      let updatedCode;
+
+      if (isTor) {
+        sourceMapURL = await createBase64SourceMapURL(sourceMapURL);
+        updatedCode = code.replace(needle, `$1${sourceMapURL}`);
+      } else {
+        updatedCode = code.replace(needle, `$1${sourceMapURL}$3`);
+      }
+
+      // eslint-disable-next-line no-eval
+      eval(updatedCode);
+    } else {
+      // eslint-disable-next-line no-eval
+      eval(code);
+    }
+  })
+  .catch((exception) => {
+    console.error(exception);
+  });
+
+/** @param {string} url */
+async function createBase64SourceMapURL(url) {
+  const response = await fetch(url);
+  const blob = await response.blob();
+
+  const jsonBlob = new Blob([blob], { type: 'application/json' });
+
+  const reader = new FileReader();
+  await new Promise((resolve, reject) => {
+    reader.addEventListener('load', resolve);
+    reader.addEventListener('error', reject);
+    reader.readAsDataURL(jsonBlob);
+  });
+
+  return /** @type {string} */ (reader.result);
+}
