27428839d4e77193a2c350384bb82b9749e4f346
JenieX <sozorablack@gmail.com>
2025-05-26 03:16:42 +0300

    🧹 add support for all browsers

--- google-play-breaker/dist/google-play-breaker.dev.js
+++ google-play-breaker/dist/google-play-breaker.dev.js
@@ -15,14 +15,57 @@
 // @license        MIT
 // ==/UserScript==
 
-let url = 'http://192.168.0.39:1013/google-play-breaker';
+const isTor = sessionStorage.getItem('isTor') === 'true';
 
-if (navigator.userAgent.includes('Firefox')) {
-  const exposerUUID = sessionStorage.getItem('exposerUUID').slice(0, -1);
-  url = exposerUUID + '/google-play-breaker/dist/google-play-breaker.bundle.js';
+const exposerUUID = sessionStorage.getItem('exposerUUID')?.slice(0, -1);
+const isExposed = exposerUUID !== undefined;
+
+let bundleURL = 'http://192.168.0.39:1013/google-play-breaker';
+
+if (isExposed) {
+  bundleURL = `${exposerUUID}/google-play-breaker/dist/google-play-breaker.bundle.js`;
 }
 
-fetch(url)
-  .then((response) => response.text())
-  .then((responseText) => eval(responseText))
-  .catch((exception) => console.error(exception));
+fetch(bundleURL)
+  .then(async (response) => response.text())
+  .then(async (code) => {
+    if (isExposed) {
+      let sourceMapURL = bundleURL.replace('bundle.js', 'source.map');
+
+      const needle = /(MappingURL=)([^?]+)([^\n]+)/;
+      let updatedCode;
+
+      if (isTor) {
+        sourceMapURL = await createBase64SourceMapURL(sourceMapURL);
+        updatedCode = code.replace(needle, `$1${sourceMapURL}`);
+      } else {
+        updatedCode = code.replace(needle, `$1${sourceMapURL}$3`);
+      }
+
+      // eslint-disable-next-line no-eval
+      eval(updatedCode);
+    } else {
+      // eslint-disable-next-line no-eval
+      eval(code);
+    }
+  })
+  .catch((exception) => {
+    console.error(exception);
+  });
+
+/** @param {string} url */
+async function createBase64SourceMapURL(url) {
+  const response = await fetch(url);
+  const blob = await response.blob();
+
+  const jsonBlob = new Blob([blob], { type: 'application/json' });
+
+  const reader = new FileReader();
+  await new Promise((resolve, reject) => {
+    reader.addEventListener('load', resolve);
+    reader.addEventListener('error', reject);
+    reader.readAsDataURL(jsonBlob);
+  });
+
+  return /** @type {string} */ (reader.result);
+}
